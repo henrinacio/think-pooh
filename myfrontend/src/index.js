9c9524c46bbe139d085817854b38316cda0ce7cc
Henrinacio <henriquedegodoy@hotmail.com>
2021-03-07 21:42:21 -0300

    Code cleanup

--- myfrontend/src/index.js
+++ myfrontend/src/index.js
@@ -1,12 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { CssBaseline } from "@material-ui/core";
+import { CssBaseline, ThemeProvider } from "@material-ui/core";
 import App from "./App";
+import theme from "./theme";
 
 ReactDOM.render(
   <React.StrictMode>
-    <CssBaseline />
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
---
+++ myfrontend/src/theme.js
@@ -0,0 +1,9 @@
+import { unstable_createMuiStrictModeTheme as createMuiTheme } from "@material-ui/core/styles";
+
+const theme = createMuiTheme({
+  typography: {
+    fontFamily: ["Mulish"],
+  },
+});
+
+export default theme;
